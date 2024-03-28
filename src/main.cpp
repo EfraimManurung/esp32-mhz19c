0f8f4e9b705b8bdb8cf34ce0343c90d70a8199ad
Efraim Manurung <frm199911@gmail.com>
2024-03-28 16:17:33 +0100

    First commit - Efraim

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/*
+  Author: Efraim Manurung
+
+  I made combinations of few repositories to made this fimrware work on ESP32.
+
+*/
+
+#include <Arduino.h>
+#include <drivers/MHZ19_uart.h>
+
+const int rx_pin = 16; //Serial rx pin no
+const int tx_pin = 17; //Serial tx pin no
+
+MHZ19_uart mhz19;
+
+void setup()
+{
+  Serial.begin(9600);
+  mhz19.begin(rx_pin, tx_pin);
+  mhz19.setAutoCalibration(false);
+
+  Serial.println("MH-Z19 is warming up now.");
+
+  // Sometimes it takes longer
+  delay(10 * 1000); 
+}
+
+void loop()
+{
+  measurement_t m = mhz19.readSerialData();
+  Serial.print("co2: ");
+  Serial.println(m.co2_ppm);
+  Serial.print("temp: ");
+  Serial.println(m.temperature);
+
+  delay(5000);
+}
